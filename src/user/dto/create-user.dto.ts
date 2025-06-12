cf666d4c0c448df74c27bd1061e79d066ff03347
Kaiser <adrielfrezatti@gmail.com>
2025-06-11 22:32:58 -0300

    dto for user and movies

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'The email cannot be empty.' })
+  @IsEmail({}, { message: 'Please provide a valid email.' })
+  email: string;
+
+  @IsOptional()
+  @IsString({ message: 'The name must be a string.' })
+  name?: string;
+
+  @IsNotEmpty({ message: 'The password cannot be empty.' })
+  @MinLength(8, { message: 'The password must be at least 8 characters long.' })
+  password: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from 'generated/prisma';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
